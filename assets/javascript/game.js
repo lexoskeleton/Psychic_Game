b01758ae7ce26772b83af70df6f604374a8c9958
lextiel <cummins.a88@gmail.com>
2018-06-09 00:10:51 -0400

    js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Create random letter generator for computer's choice
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "x"]
+//Create variable capturing user's guess via click event
+var userGuess = "";
+
+// Creating variables to hold the number of wins & lossesThey start at 0.
+
+//create a function that runs whenever the user presses a key
+document.onkeyup = function(event) {
+    userGuess.textContent = event.key;
+  };
+  if (userGuess) = 
+  
+//Create conditionals the return either wins or losses
+//Create a counter of how many guesses the user has made
+//Create a decrement function for how many guesses the user has left
